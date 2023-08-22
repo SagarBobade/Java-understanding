7308e67ed83e836ff5af9b1248a4ceca05daefec
Sagar-Bobade <sagar.b@phynart.com>
2023-08-22 22:49:36 +0530

    accepted string from user instead of hardcode

--- src/strings/ReverseStringWithoutBuiltIn.java
+++ src/strings/ReverseStringWithoutBuiltIn.java
@@ -2,20 +2,24 @@ package strings;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ReverseStringWithoutBuiltIn {
 
 	public static void main(String[] args) {
 
-		String name = "Ankush";
+		System.out.println("Enter a String to reverse");
+		Scanner sc = new Scanner(System.in);
+		
+		String myString = sc.next();
+		
 		List<Character> charList = new ArrayList<>();
 
-		for (char c : name.toCharArray()) {
-			System.out.println(c);
+		for (char c : myString.toCharArray()) {
+		//	System.out.println(c);
 			charList.add(c);
 		}
 
-		System.out.println(charList.size());
 		for(int i=charList.size()-1; i>=0; i--) {
 			System.out.print(charList.get(i));
 		}
